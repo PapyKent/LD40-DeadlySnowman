4aaab06d6dc3474613badc0e3048bef182ae2268
Thomas <thomasjamestiamlee@gmail.com>
2017-12-02 15:57:51 +0900

    Added methods for changing the size of the snowball.

--- Deadly Snowman/Assets/Scripts/PlayerController.cs
+++ Deadly Snowman/Assets/Scripts/PlayerController.cs
@@ -8,15 +8,38 @@ public class PlayerController : MonoBehaviour {
 	public float growthRate;
 	private Rigidbody rb;
 
+	private float targetScale = -1f;
+
 	void Start () {
 		rb = GetComponent <Rigidbody> ();
+		targetScale = transform.localScale.y;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.X))
+			changeScale (transform.localScale.y + 2f);
+		if (Input.GetKeyDown(KeyCode.Z))
+			changeScale (transform.localScale.y - 2f);
 	}
 	
 	void FixedUpdate () {
+		if (transform.localScale.y != targetScale) {
+			float newValue = 0f;
+			if (transform.localScale.y < targetScale) {
+				newValue = Mathf.Clamp (transform.localScale.y + growthRate, transform.localScale.y, targetScale);
+			} else {
+				newValue = Mathf.Clamp (transform.localScale.y - growthRate, targetScale, transform.localScale.y);
+			}
+			transform.localScale = new Vector3 (newValue, newValue, newValue);
+		}
+
 		float moveHorizontal = Input.GetAxis ("Horizontal");
-
 		Vector3 force = new Vector3 (moveHorizontal * speed, 0f, 0f);
-
 		rb.AddForce (force);
+	}
+
+	/* Changes the size of the snowball. */
+	private void changeScale(float scale) {
+		targetScale = scale;
 	}
 }
