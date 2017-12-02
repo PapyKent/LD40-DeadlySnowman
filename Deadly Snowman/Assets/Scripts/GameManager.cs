8bb18a8a6e257d36a43257221159b493dbb87223
PapyKent <kentdu38@gmail.com>
2017-12-02 14:33:21 +0900

    hierarchy

---
+++ Deadly Snowman/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public float timer = 0;
+	float ballSize = 1;
+	public int ballContent = 0;
+	float startTimer=0;
+	float endTimer=0;
+	bool gameOver = false;
+
+
+	// Use this for initialization
+	void Start () {
+		startTimer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//end of the game
+		if (Input.GetKeyUp ("space") && !gameOver) {
+			endTimer = Time.time;
+			timer = (endTimer - startTimer);
+			print ("Game over! Time is :" + timer.ToString() + "sec");
+			float score = timer + ballSize + ballContent;
+			print ("Score is :" + score + "sec");
+			gameOver = true;
+		}
+	}
+}
