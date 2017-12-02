8da7fe66fc73c7d36b1ead82e20c091a1187dd6c
Thomas <thomasjamestiamlee@gmail.com>
2017-12-02 14:50:13 +0900

    Added basic movement scene.

---
+++ Deadly Snowman/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent <Rigidbody> ();
+	}
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 force = new Vector3 (moveHorizontal, 0f, 0f);
+
+		rb.AddForce (force);
+	}
+}
