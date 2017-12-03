d04d89b1ca2fe2b08a50670a78a6bf2287b156aa
Tomohiro118 <b1014218@fun.ac.jp>
2017-12-03 17:21:55 +0900

    Characters and some scripts for them

---
+++ Deadly Snowman/Assets/TomohiroAsset/Scripts/Skier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skier : MonoBehaviour
+{
+
+    public float speed;
+
+    Animator animator;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        float velocity = rb.velocity.magnitude;
+        Debug.Log(velocity);
+
+        if(velocity > speed)
+        {
+            animator.SetTrigger("Blown");
+        }
+    }
+}
+
